b0f9d20a0d60738003320b1609f45ac4295292a5
Enrique Carrillo <niofis@debian>
2015-05-12 07:18:18 +0000

    Small update.

--- bubble.hs
+++ bubble.hs
@@ -1,13 +1,12 @@
 bubble :: (Ord a) => [a] -> [a]
-bubble a = bubble' [] a 0
+bubble a = bubble' [] a False
   where
     bubble' prev next mov
-      | (length next) < 2 = if mov == 1 then bubble prev ++ next  else prev ++ next
-      | h > t = bubble' (prev ++ [t])  ([h] ++ (drop 2 next)) 1
+      | (length next) < 2 = if mov == True then bubble prev ++ next  else prev ++ next
+      | h > t = bubble' (prev ++ [t])  ([h] ++ (drop 2 next)) True
       | otherwise = bubble' (prev ++ [h]) (drop 1 next) mov
         where
-          temp = (take 2 next)
-          h = (head temp)
-          t = (head (tail temp)) 
+          h = (head next)
+          t = (head (tail next)) 
        
 main = print $ bubble [4,5,5,7,8,9,2,5,0]  
