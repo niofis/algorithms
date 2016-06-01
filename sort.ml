5d291d57b643c61fc3aaa8b9f42dfcee98006413
Enrique <niofis@gmail.com>
2016-06-01 08:13:02 -0700

    merge sort added

--- sort.ml
+++ sort.ml
@@ -6,14 +6,12 @@ let rec print_list = function
 [] -> ()
 | e::l -> print_int e ; print_string " " ; print_list l
 
-let rec qsort = function
+let rec quick_sort = function
   | [] -> []
   | h::t -> (qsort (List.filter (fun x -> x < h) t))@[h]@(qsort (List.filter (fun y -> y >= h) t))
 
-let rec msort l = match l with
-  [] -> []
-  | _ -> 
-  let rec extract s e l = let () = Printf.printf "(%i %i %i) " s e (List.length l) in match s,e with
+let rec merge_sort l =
+  let rec extract s e l = match s,e with
   | s, e when s < e -> (List.nth l s)::extract (s+1) e l
   | _ -> []
   in
@@ -22,6 +20,9 @@ let rec msort l = match l with
   | a, [] -> a
   | (a::aa), (b::bb) -> (if a < b then a::(merge aa (b::bb)) else b::(merge (a::aa) bb))
   in
-  let left = extract 0 ((List.length l |> float_of_int) /. 2. +. 1.|> floor |> truncate) l in
-  let right = extract ((List.length l |> float_of_int) /. 2. |> ceil |> truncate) (List.length l) l in
-  merge (msort left) (msort right)
+  if List.length l < 2 then
+    l
+  else
+    let left = extract 0 ((List.length l + 1 |> float_of_int) /. 2.|> floor |> truncate) l in
+    let right = extract ((List.length l |> float_of_int) /. 2. |> ceil |> truncate) (List.length l) l in
+    merge (merge_sort left) (merge_sort right)
